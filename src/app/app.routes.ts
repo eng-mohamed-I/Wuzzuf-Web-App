2be97e93ceef4cdcf096b5b0e3e6b8487cc25432
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-10-13 00:45:09 +0300

    login page styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent },
+];
