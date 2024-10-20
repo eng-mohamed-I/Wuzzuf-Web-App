ac67c55399a459228956f1c695d8b914250bca34
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-10-20 22:05:44 +0300

    edits on register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  passwordVisibility = 'password';
+
+  constructor() {}
+
+  changePasswordVisibility() {
+    this.passwordVisibility =
+      this.passwordVisibility === 'password' ? 'text' : 'password';
+  }
+}
