9f7c013c60741edff51680d060952a668a6b5e1b
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-10-21 23:48:29 +0300

    init jobs page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'jobs/egypt', pathMatch: 'full' },
+  { path: 'jobs/egypt', component: JobsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundComponent },
---
+++ src/app/pages/jobs/jobs-header/jobs-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './jobs-header.component.html',
+  styleUrl: './jobs-header.component.css'
+})
+export class JobsHeaderComponent {
+
+}
