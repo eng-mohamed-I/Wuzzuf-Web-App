b4703c7e70086307b1abfd76146170c7b53f9248
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-10-22 18:59:43 +0300

    init header in jobs page

--- src/app/pages/jobs/jobs-header/jobs-header.component.ts
+++ src/app/pages/jobs/jobs-header/jobs-header.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-jobs-header',
   standalone: true,
   imports: [],
   templateUrl: './jobs-header.component.html',
-  styleUrl: './jobs-header.component.css'
+  styleUrl: './jobs-header.component.css',
 })
-export class JobsHeaderComponent {
+export class JobsHeaderComponent implements OnInit {
+  searchPlaceholder: string[] = [
+    'Search Jobs (e.g. Work From Home)',
+    'Search Jobs (e.g. Senior PHP developer)',
+    'Search Jobs (e.g. Sales in Maadi)',
+  ];
+  counter: number = 0;
 
+  constructor(@Inject(PLATFORM_ID) private _paltformId: object) {
+    // this.displayCounter();
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this._paltformId)) {
+      this.displayCounter();
+    }
+  }
+
+  displayCounter() {
+    setInterval(() => {
+      this.incrementCounter();
+    }, 5000);
+  }
+
+  incrementCounter() {
+    this.counter++;
+    this.counter === this.searchPlaceholder.length ? (this.counter = 0) : '';
+  }
 }
