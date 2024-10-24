4b30c52648bd7cdebd9db7ac3f21194c79fe0d64
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-10-24 21:11:54 +0300

    browser jobs setting visibilty

---
+++ src/app/components/navbar/browse-jobs/browse-jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-jobs',
+  standalone: true,
+  imports: [],
+  templateUrl: './browse-jobs.component.html',
+  styleUrl: './browse-jobs.component.css'
+})
+export class BrowseJobsComponent {
+
+}
