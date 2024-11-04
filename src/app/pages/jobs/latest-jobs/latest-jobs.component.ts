e60b8d13035f94249ca42528a17543f6cd5f664f
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-11-04 18:29:55 +0200

    init latest jobs

---
+++ src/app/pages/jobs/latest-jobs/latest-jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-jobs',
+  standalone: true,
+  imports: [],
+  templateUrl: './latest-jobs.component.html',
+  styleUrl: './latest-jobs.component.css'
+})
+export class LatestJobsComponent {
+
+}
